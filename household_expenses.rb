85b88e698320ebfc26e371c547358cae3f0546f7
zerolive <zerolive@gmail.com>
2017-06-23 19:38:46 +0200

    Match routes with or without slash

--- household_expenses.rb
+++ household_expenses.rb
@@ -5,9 +5,9 @@ class HouseholdExpenses
   def call(env)
     path = env['PATH_INFO']
     params = Rack::Utils.parse_nested_query(env['QUERY_STRING'])
-    return [200, {}, [readme_to_s]] if path == '/'
-    return [200, {}, [login_page]] if path == '/login'
-    return [200, {}, [home_page(params)]] if path == '/home'
+    return [200, {}, [readme_to_s]] if match?(path, '/')
+    return [200, {}, [login_page]] if match?(path, '/login')
+    return [200, {}, [home_page(params)]] if match?(path, '/home')
     return [404, {}, ['page not found']]
   end
 
@@ -33,6 +33,10 @@ class HouseholdExpenses
   def conclusive?(data)
     conclusion = LoginService.new(data['email'], data['password']).conclude
     conclusion['message'] == 'fail'
+  end
+
+  def match?(path, route)
+    path == route || path == (route + '/')
   end
 
   def readme_to_s
